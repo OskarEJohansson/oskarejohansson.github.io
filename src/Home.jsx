da9a377651788f361b7989957b9950e2a0b3af90
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-02-15 10:31:11 +0100

    simplifying the download resume button

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,13 @@
 import experiencePNG from "./media/experience.png";
+import experiencePDF from "./media/OskarJohanssonResume.pdf";
 
 const Home = () => {
-  const handleDownloadClick = () => {
-    const pdfUrl =
-      "https://drive.google.com/file/d/1F83C2RzfpTfrNTyMdsSC2VP4ETNUuW43/view?usp=share_link";
-
-    document.body
-      .appendChild(
-        Object.assign(document.createElement("a"), {
-          href: pdfUrl,
-          download: "OskarJohanssonResume.pdf",
-        })
-      )
-      .click();
-  };
-
   return (
     <div id="home">
       <img src={experiencePNG} id="home-img" alt="Picture" />
-      <button id="button-resume" onClick={handleDownloadClick}>
-        Download
-      </button>
+      <a href={experiencePDF} download="OskarJohanssonResume.pdf">
+        <button id="button-resume">Download</button>
+      </a>
     </div>
   );
 };
