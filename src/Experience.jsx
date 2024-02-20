8dc5158579c1c67987bf27c77a165c5ad7b67ec6
Oskar Johanssonlsclear <133226269+OskarEJohansson@users.noreply.github.com>
2024-02-20 07:42:35 +0100

    updated experience and Resume

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -11,17 +11,55 @@ const Experience = () => {
   };
 
   return (
-    <section id="experience">
-      <div>
-        <nav id="experience-nav">
-          <FaLinkedinIn
-            size={100}
-            className="icons"
-            onClick={handleLinkedinOnClick}
-          />
-          <FaGithub size={100} onClick={handleGitOnClick} className="icons" />
-        </nav>
-      </div>
+    <section className="experience">
+      <nav className="experience">
+        <div className="experience">
+          <fieldset className="experience">
+            <button className="icons">
+              <FaGithub
+                size={100}
+                onClick={handleGitOnClick}
+                className="icons"
+              />
+            </button>
+            <p className="eperience-p">
+              Please visit my Github repository to follow my progress through my
+              projects from my first ever Python project and my school projects
+              "Dice" and "Dungeon Run". Both school projects are done with
+              different techniques and show my progress from justing just plain
+              Java to connect to a Database through JDBC followed by using
+              Spring Boot to connect and handle services.
+              <br /> <br />
+              You can also find the repository for this website. More projects
+              in Javascript/Typescript will be uploaded as our school year
+              progresses and my knowledge of the front end aspect of the stack
+              will grow. Beside school projects I will post my own projects to
+              showcase my interesset in exploring the vast universe of
+              development. As of the start of 2024 I'm taking MongoDB University
+              courses to learn more about MongoDB and I will also keep on
+              exploring Docker and the use of containers in back end
+              applications.
+            </p>
+          </fieldset>
+        </div>
+
+        <div className="experience">
+          <fieldset className="experience">
+            <button className="icons">
+              <FaLinkedinIn
+                size={100}
+                className="icons"
+                onClick={handleLinkedinOnClick}
+              />
+            </button>
+
+            <p className="eperience-p">
+              Please visit my Linkedin profile for more information of my
+              earlier work experience and to connect to my Linkedin profile!
+            </p>
+          </fieldset>
+        </div>
+      </nav>
     </section>
   );
 };
